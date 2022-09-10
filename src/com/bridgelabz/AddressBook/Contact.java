40e0485d8ccc06fbf28c7d090ea04deb3a5df047
RanjitsinhMule <muleranjitsinh7323@gmail.com>
2022-09-10 09:33:07 +0530

    Edit contact

--- src/com/bridgelabz/AddressBook/Contact.java
+++ src/com/bridgelabz/AddressBook/Contact.java
@@ -11,7 +11,7 @@ public class Contact {
 		Scanner sc = new Scanner(System.in);
 		while (true) {
 			System.out.println("Welcome To Address Book System\n\n" + "Choose your option:\n"
-					+ "1. Create new contact\n" + "2. View all the existing contacts\n");
+					+ "1. Create new contact\n" + "2. View all the existing contacts\n" + "3. Edit existing contact\n");
 			int option = sc.nextInt();
 			sc.nextLine();
 			switch (option) {
@@ -54,7 +54,21 @@ public class Contact {
 				System.out.println("\n\n");
 				break;
 			// edit existing contact
-			
+			case 3:
+				System.out.println("Edit record");
+				System.out.println("Enter name to edit:");
+				for (int i = 0; i < person.size(); i++) {
+					address = person.get(i);
+
+					if (sc.nextLine().equals(address.getFirstName())) {
+						System.out.println("Enter new city:");
+						address.setCity(sc.nextLine());
+						System.out.println("List After is" + person.toString());
+					} else {
+						System.out.println("User not found");
+					}
+				}
+
 			case 5:
 				return;
 			}
